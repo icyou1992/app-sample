3dadfe339f7d87700850ec9ee354d1ca99821f99
icyou1992 <icyou1992@gmail.com>
2023-09-10 13:20:47 +0900

    create react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>PFe DEMO APP</code> 
+        </p>
+        <div>asdasd</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
